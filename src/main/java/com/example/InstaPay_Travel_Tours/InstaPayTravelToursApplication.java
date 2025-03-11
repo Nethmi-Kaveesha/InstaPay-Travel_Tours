dbdb3de44e2848a056af16193f2e22b3e7cc944d
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-03-11 00:22:36 -0700

    start InstaPay-Travel_Tours

---
+++ src/main/java/com/example/InstaPay_Travel_Tours/InstaPayTravelToursApplication.java
@@ -0,0 +1,13 @@
+package com.example.InstaPay_Travel_Tours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaPayTravelToursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaPayTravelToursApplication.class, args);
+	}
+
+}
