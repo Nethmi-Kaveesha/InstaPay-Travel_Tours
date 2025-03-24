5e50fa6deaf041c24870afbbf7b9658a492614a7
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-03-24 14:27:14 -0700

    create js to language-translator.js

--- InstaPay-Travel_Tours-Frontend/js/language-translator.js
+++ InstaPay-Travel_Tours-Frontend/js/language-translator.js
@@ -1,7 +1,31 @@
 const selectTag = document.querySelectorAll("select");
+const translateBtn = document.querySelector("button");
+const fromText = document.querySelector(".from-text"); // Selects only one textarea
+const toText = document.querySelector(".to-text");
 
-selectTag.forEach(tag => {
-    for (const key in object){
-
+// Populate language options
+selectTag.forEach((tag, id) => {
+    for (const country_code in countries) {
+        let selected = "";
+        if (id === 0 && country_code === "en-GB") {
+            selected = "selected";
+        } else if (id === 1 && country_code === "hi-IN") {
+            selected = "selected";
+        }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
     }
-})
+});
+
+// Translate button event listener
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+        translateFrom = selectTag[0].value,
+        translateTo = selectTag[1].value;
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        console.log(data);
+        toText.value = data.responseData.translatedText;
+    })
+
+});
