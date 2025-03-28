9b7d39429efa648cd5dc3e82b00c78656aae034c
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-03-27 23:46:18 -0700

    create repositories

---
+++ src/main/java/com/example/InstaPay_Travel_Tours/repo/BookingDetailRepository.java
@@ -0,0 +1,7 @@
+package com.example.InstaPay_Travel_Tours.repo;
+
+import com.example.InstaPay_Travel_Tours.entity.BookingDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingDetailRepository extends JpaRepository<BookingDetail,Integer> {
+}
--- src/main/java/com/example/InstaPay_Travel_Tours/service/PlaceBookingService.java
+++ src/main/java/com/example/InstaPay_Travel_Tours/service/PlaceBookingService.java
@@ -1,9 +1,7 @@
-//package com.example.InstaPay_Travel_Tours.service;
-//
-//import com.example.InstaPay_Travel_Tours.dto.PaymentDTO;
-//import com.example.InstaPay_Travel_Tours.dto.BookingDTO;
-//
-//public interface PlaceBookingService {
-//
-//    boolean addBooking(BookingDTO bookingDTO, PaymentDTO paymentDTO);
-//}
+package com.example.InstaPay_Travel_Tours.service;
+
+import com.example.InstaPay_Travel_Tours.dto.BookingDTO;
+
+public interface PlaceBookingService {
+    public boolean addBooking(BookingDTO bookingDTO);
+}
