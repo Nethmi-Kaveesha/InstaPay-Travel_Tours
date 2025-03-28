36c858a1b97b4c03fef0fb3db92bb9011b8dfa1c
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-03-28 03:42:46 -0700

    add PaymentRepository

--- src/main/java/com/example/InstaPay_Travel_Tours/repo/PaymentRepository.java
+++ src/main/java/com/example/InstaPay_Travel_Tours/repo/PaymentRepository.java
@@ -3,5 +3,6 @@ package com.example.InstaPay_Travel_Tours.repo;
 import com.example.InstaPay_Travel_Tours.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+    // Additional query methods (if necessary)
 }
