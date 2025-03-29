0b4dad56cdec0f27b41d67fe91158e73845f9ac7
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-03-29 04:10:40 -0700

    comment Payment.java

--- src/main/java/com/example/InstaPay_Travel_Tours/entity/Payment.java
+++ src/main/java/com/example/InstaPay_Travel_Tours/entity/Payment.java
@@ -1,104 +1,104 @@
-package com.example.InstaPay_Travel_Tours.entity;
-
-import jakarta.persistence.*;
-import java.time.LocalDate;
-
-@Entity
-@Table(name = "payment")
-public class Payment {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int paymentId;
-
-    @ManyToOne
-    @JoinColumn(name = "booking_id", nullable = false)
-    private Booking booking;
-
-    private double amountPaid;
-    private LocalDate paymentDate;
-    private String paymentMethod;
-    private String cardNumber;
-    private String cardExpiry;
-    private String cardCvv;
-
-    // Default constructor
-    public Payment() {
-    }
-
-    // Constructor to initialize the payment details
-    public Payment(Booking booking, double amountPaid, LocalDate paymentDate, String paymentMethod, String cardNumber, String cardExpiry, String cardCvv) {
-        this.booking = booking;
-        this.amountPaid = amountPaid;
-        this.paymentDate = paymentDate;
-        this.paymentMethod = paymentMethod;
-        this.cardNumber = cardNumber;
-        this.cardExpiry = cardExpiry;
-        this.cardCvv = cardCvv;
-    }
-
-    // Getters and Setters
-    public int getPaymentId() {
-        return paymentId;
-    }
-
-    public void setPaymentId(int paymentId) {
-        this.paymentId = paymentId;
-    }
-
-    public Booking getBooking() {
-        return booking;
-    }
-
-    public void setBooking(Booking booking) {
-        this.booking = booking;
-    }
-
-    public double getAmountPaid() {
-        return amountPaid;
-    }
-
-    public void setAmountPaid(double amountPaid) {
-        this.amountPaid = amountPaid;
-    }
-
-    public LocalDate getPaymentDate() {
-        return paymentDate;
-    }
-
-    public void setPaymentDate(LocalDate paymentDate) {
-        this.paymentDate = paymentDate;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public String getCardExpiry() {
-        return cardExpiry;
-    }
-
-    public void setCardExpiry(String cardExpiry) {
-        this.cardExpiry = cardExpiry;
-    }
-
-    public String getCardCvv() {
-        return cardCvv;
-    }
-
-    public void setCardCvv(String cardCvv) {
-        this.cardCvv = cardCvv;
-    }
-}
+//package com.example.InstaPay_Travel_Tours.entity;
+//
+//import jakarta.persistence.*;
+//import java.time.LocalDate;
+//
+//@Entity
+//@Table(name = "payment")
+//public class Payment {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int paymentId;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "booking_id", nullable = false)
+//    private Booking booking;
+//
+//    private double amountPaid;
+//    private LocalDate paymentDate;
+//    private String paymentMethod;
+//    private String cardNumber;
+//    private String cardExpiry;
+//    private String cardCvv;
+//
+//    // Default constructor
+//    public Payment() {
+//    }
+//
+//    // Constructor to initialize the payment details
+//    public Payment(Booking booking, double amountPaid, LocalDate paymentDate, String paymentMethod, String cardNumber, String cardExpiry, String cardCvv) {
+//        this.booking = booking;
+//        this.amountPaid = amountPaid;
+//        this.paymentDate = paymentDate;
+//        this.paymentMethod = paymentMethod;
+//        this.cardNumber = cardNumber;
+//        this.cardExpiry = cardExpiry;
+//        this.cardCvv = cardCvv;
+//    }
+//
+//    // Getters and Setters
+//    public int getPaymentId() {
+//        return paymentId;
+//    }
+//
+//    public void setPaymentId(int paymentId) {
+//        this.paymentId = paymentId;
+//    }
+//
+//    public Booking getBooking() {
+//        return booking;
+//    }
+//
+//    public void setBooking(Booking booking) {
+//        this.booking = booking;
+//    }
+//
+//    public double getAmountPaid() {
+//        return amountPaid;
+//    }
+//
+//    public void setAmountPaid(double amountPaid) {
+//        this.amountPaid = amountPaid;
+//    }
+//
+//    public LocalDate getPaymentDate() {
+//        return paymentDate;
+//    }
+//
+//    public void setPaymentDate(LocalDate paymentDate) {
+//        this.paymentDate = paymentDate;
+//    }
+//
+//    public String getPaymentMethod() {
+//        return paymentMethod;
+//    }
+//
+//    public void setPaymentMethod(String paymentMethod) {
+//        this.paymentMethod = paymentMethod;
+//    }
+//
+//    public String getCardNumber() {
+//        return cardNumber;
+//    }
+//
+//    public void setCardNumber(String cardNumber) {
+//        this.cardNumber = cardNumber;
+//    }
+//
+//    public String getCardExpiry() {
+//        return cardExpiry;
+//    }
+//
+//    public void setCardExpiry(String cardExpiry) {
+//        this.cardExpiry = cardExpiry;
+//    }
+//
+//    public String getCardCvv() {
+//        return cardCvv;
+//    }
+//
+//    public void setCardCvv(String cardCvv) {
+//        this.cardCvv = cardCvv;
+//    }
+//}
