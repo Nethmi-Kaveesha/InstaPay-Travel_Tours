89fd653ab8b9a3983301453c69d5fef94e151679
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-04-16 03:03:30 -0700

    update PlaceBookingService.java

--- src/main/java/com/example/InstaPay_Travel_Tours/service/PlaceBookingService.java
+++ src/main/java/com/example/InstaPay_Travel_Tours/service/PlaceBookingService.java
@@ -2,6 +2,10 @@ package com.example.InstaPay_Travel_Tours.service;
 
 import com.example.InstaPay_Travel_Tours.dto.BookingDTO;
 
+import java.util.List;
+
 public interface PlaceBookingService {
     public boolean addBooking(BookingDTO bookingDTO);
+
+    List<BookingDTO> getAllBookings();
 }
