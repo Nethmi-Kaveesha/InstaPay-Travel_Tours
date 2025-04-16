c16889170885bcc365e45b19a97db6d02e14c5e7
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-04-16 03:08:55 -0700

    add TransferRepository.java

---
+++ src/main/java/com/example/InstaPay_Travel_Tours/repo/TransferRepository.java
@@ -0,0 +1,7 @@
+package com.example.InstaPay_Travel_Tours.repo;
+
+import com.example.InstaPay_Travel_Tours.entity.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+}
