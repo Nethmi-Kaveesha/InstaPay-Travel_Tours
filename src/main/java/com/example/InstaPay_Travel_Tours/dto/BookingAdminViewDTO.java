b165f19e66cd688bc8716b2157ff32f9df641a9f
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-04-16 02:54:01 -0700

    update BookingAdminViewDTO

---
+++ src/main/java/com/example/InstaPay_Travel_Tours/dto/BookingAdminViewDTO.java
@@ -0,0 +1,33 @@
+package com.example.InstaPay_Travel_Tours.dto;
+
+import java.util.Date;
+
+public class BookingAdminViewDTO {
+
+    private int bookingId;
+    private Date bookingDate;
+    private double totalPrice;
+    private String userId;
+    private int detailId;
+    private int tourId;
+    private double price;
+    private double quantity;
+    private double total;
+
+    public BookingAdminViewDTO() {}
+
+    public BookingAdminViewDTO(int bookingId, Date bookingDate, double totalPrice, String userId,
+                               int detailId, int tourId, double price, double quantity, double total) {
+        this.bookingId = bookingId;
+        this.bookingDate = bookingDate;
+        this.totalPrice = totalPrice;
+        this.userId = userId;
+        this.detailId = detailId;
+        this.tourId = tourId;
+        this.price = price;
+        this.quantity = quantity;
+        this.total = total;
+    }
+
+    // Getters and Setters ...
+}
