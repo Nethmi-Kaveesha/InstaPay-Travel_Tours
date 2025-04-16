968e94fb9a37a927a58b31ea94a9f101c395a9a9
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-04-16 03:02:19 -0700

    update Payment.java

--- src/main/java/com/example/InstaPay_Travel_Tours/model/Payment.java
+++ src/main/java/com/example/InstaPay_Travel_Tours/model/Payment.java
@@ -1,16 +1,23 @@
 package com.example.InstaPay_Travel_Tours.model;
 
+import com.example.InstaPay_Travel_Tours.entity.Tour;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 public class Payment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String email;
     private String paymentId;
     private String status;
     private Double amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tour_id", referencedColumnName = "tourid")
+    private Tour tour;
 }
