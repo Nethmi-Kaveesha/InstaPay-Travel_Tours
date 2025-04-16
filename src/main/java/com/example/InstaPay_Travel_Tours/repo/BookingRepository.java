f1b69ea88ef3e35d7849bb303422a28c0082a821
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-04-16 02:54:44 -0700

    update BookingRepository

--- src/main/java/com/example/InstaPay_Travel_Tours/repo/BookingRepository.java
+++ src/main/java/com/example/InstaPay_Travel_Tours/repo/BookingRepository.java
@@ -3,5 +3,10 @@ package com.example.InstaPay_Travel_Tours.repo;
 import com.example.InstaPay_Travel_Tours.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
 public interface BookingRepository extends JpaRepository<Booking,Integer> {
+
 }
